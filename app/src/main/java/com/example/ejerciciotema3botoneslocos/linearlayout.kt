a936e886cd587c38870152e73de0cd303cac6375
MiguelMataRamos <131800578+MiguelMataRamos@users.noreply.github.com>
2023-10-16 04:51:17 +0200

    empezando el linear

--- app/src/main/java/com/example/ejerciciotema3botoneslocos/linearlayout.kt
+++ app/src/main/java/com/example/ejerciciotema3botoneslocos/linearlayout.kt
@@ -3,9 +3,9 @@ package com.example.ejerciciotema3botoneslocos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class linearlayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.constraintlayout)
+        setContentView(R.layout.activity_linearlayout)
     }
 }
